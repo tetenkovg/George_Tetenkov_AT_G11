430016e988e52ae677d7c893904129b9f835dfcf
George Tetenkov <george.tetenkov@gmail.com>
2024-11-11 14:48:26 +0100

    implement Cow

--- src/main/java/homework/day2/Art.java
+++ src/main/java/homework/day2/Art.java
@@ -2,7 +2,7 @@ package homework.day2;
 
 public class Art {
     public static void main(String[] args) {
-
+        //Cow
         System.out.println("  ^__^");
         System.out.println("  (oo)\\_______");
         System.out.println("  (__)\\ ) \\/\\  \\");
