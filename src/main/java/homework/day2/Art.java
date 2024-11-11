96ef73e63b64550d6614101a99146c6ce127a158
George Tetenkov <george.tetenkov@gmail.com>
2024-11-11 14:47:28 +0100

    implement sumOfElementsDivisibleByFirstDigitOfLength

---
+++ src/main/java/homework/day2/Art.java
@@ -0,0 +1,12 @@
+package homework.day2;
+
+public class Art {
+    public static void main(String[] args) {
+
+        System.out.println("  ^__^");
+        System.out.println("  (oo)\\_______");
+        System.out.println("  (__)\\ ) \\/\\  \\");
+        System.out.println("       ||----w |\\");
+        System.out.println("       ||     ||");
+    }
+}
