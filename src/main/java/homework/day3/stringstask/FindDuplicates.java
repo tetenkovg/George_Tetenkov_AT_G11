2cf02c86ba978123cbec666a41fbb1b7e7e4be7e
George Tetenkov <george.tetenkov@gmail.com>
2024-11-18 20:26:54 +0100

    3.2

--- src/main/java/homework/day3/stringstask/FindDuplicates.java
+++ src/main/java/homework/day3/stringstask/FindDuplicates.java
@@ -1,9 +1,32 @@
 package homework.day3.stringstask;
 
 public class FindDuplicates {
+    public static void main(String[] args) {
+        FindDuplicates finder = new FindDuplicates();
+        finder.findDuplicates("hello world hello java java java world");
+    }
+
     void findDuplicates (String text){
-        String[] array = text.split("\\s+");
+        String[] words = text.split("\\s+");
 
+        for (int i = 0; i < words.length; i++) {
+            if (words[i] ==  null) {
+                continue;
+            }
 
+            int count = 1;
+
+            for (int j = i +1; j < words.length; j++) {
+                if (words[j] != null && words[i].equals(words[j])) {
+                    count++;
+                    words[j] = null;
+                }
+            }
+
+                if (count > 1){
+                    System.out.printf("Слово '%s' встречается %d раз(а)%n", words[i], count);
+                }
+            words[i] = null;
+        }
     }
 }
