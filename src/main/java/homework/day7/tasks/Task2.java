75e09a944868ea043f1a8774036d82ebd5e2f0e1
George Tetenkov <george.tetenkov@gmail.com>
2024-11-27 15:39:36 +0100

    homework 7 task 6 and Writing in file for task2 and task4

--- src/main/java/homework/day7/tasks/Task2.java
+++ src/main/java/homework/day7/tasks/Task2.java
@@ -1,5 +1,8 @@
 package homework.day7.tasks;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,6 +11,28 @@ public class Task2 {
 
     public static void main(String[] args) {
         List<String> figures = Arrays.asList("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс");
+
+        String fileName = "figures.txt";
+
+        try  {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+
+            StringBuilder sb = new StringBuilder();
+            for (String figure : figures) {
+                sb.append(figure).append("-");
+            }
+
+            if (sb.length() > 0) {
+                sb.setLength(sb.length() - 1);
+            }
+
+            writer.write(sb.toString());
+            writer.close();
+            System.out.println("I wrote this in file" + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     int count = 0;
 
--- src/main/java/homework/day7/tasks/Task4.java
+++ src/main/java/homework/day7/tasks/Task4.java
@@ -1,6 +1,10 @@
 package homework.day7.tasks;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Task4 {
@@ -19,11 +23,31 @@ public class Task4 {
         cars.add("Жигуль");
         cars.add("Ауди");
 
+
+        String file = "cars.txt";
+
+        try{
+            BufferedWriter write = new BufferedWriter(new FileWriter(file));
+            StringBuilder stringBuilder = new StringBuilder();
+
+
+            for (String car : cars){
+                stringBuilder.append("\"" + car + "\"" );
+            }
+            write.write(stringBuilder.toString());
+            write.close();
+            System.out.println("Записал");
+        } catch (IOException e){
+           e.printStackTrace();
+        }
+
+        List<String> filterCars = new ArrayList<>();
         for (String car : cars){
-            if (car.length() > 3){
-                cars.remove(car);
+            if (car.length() <= 4){
+                filterCars.add(car);
             }
         }
+        cars = filterCars;
 
         for (String car : cars){
             System.out.print(car + " ");
