ee77b9454f4a87333338382c691658a9f2d51059
George Tetenkov <george.tetenkov@gmail.com>
2024-12-04 22:11:56 +0100

    classwork 10

---
+++ src/main/java/classwork/day10/ClassWorkDay10.java
@@ -0,0 +1,47 @@
+package classwork.day10;
+
+import static java.lang.Math.atan;
+import static java.lang.Math.tan;
+
+public class ClassWorkDay10 {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Object lock = new Object();
+
+
+        new Thread(() -> {
+
+            synchronized (lock) {
+                for (int i = 0; i < 3; i++) {
+                    try {
+                        Thread.currentThread().sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.printf("t1-%s  ", i);
+                }
+            }
+        }).start();
+
+        Thread.sleep(500);
+
+        synchronized (lock) {
+            for (int i = 0; i < 3; i++) {
+                Thread.currentThread().sleep(1000);
+                System.out.printf("m-%s  ", i);
+            }
+        }
+
+
+    }
+
+    public static void heavyMethod() {
+        long t0 = System.currentTimeMillis();
+        for (int i = 0; i < 1_00_000_000; i++) {
+            double d = tan(atan(123456789.123456789));
+        }
+        System.out.printf("I am %s, and I have finished in %s millis \r\n", Thread.currentThread().getName(), (System.currentTimeMillis() - t0));
+    }
+}
