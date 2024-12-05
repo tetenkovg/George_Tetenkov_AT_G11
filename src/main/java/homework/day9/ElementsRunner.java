42c660298b9358317d3b4014e5f497b144a5c662
George Tetenkov <george.tetenkov@gmail.com>
2024-12-05 22:23:02 +0100

    homework 9 task 7

--- src/main/java/homework/day9/ElementsRunner.java
+++ src/main/java/homework/day9/ElementsRunner.java
@@ -1,5 +1,6 @@
 package homework.day9;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class ElementsRunner {
@@ -10,9 +11,16 @@ public class ElementsRunner {
 //    Оставить в потоке только уникальные значения
 //    Отпечатать в консоль с новой строки
 
-//    public static void main(String[] args) {
-//        Stream.of("Text field", "Radio", "Check-box", "Drop-down", "Picker", "Breadcrumb")
-//                .filter(element -> element)
-//
-//    }
+        public static void main(String[] args) {
+            Stream.of("Text field", "Radio", "Check-box", "Drop-down", "Picker", "Breadcrumb")
+                    .flatMap(element -> Arrays.stream(element.split(" ")))
+                    .map(word -> word.length() % 2 != 0
+                            ? word.replace("e", "o")
+                            : String.valueOf(word.length()))
+                    .distinct()
+                    .forEach(System.out::println);
+        }
+
 }
+
+
