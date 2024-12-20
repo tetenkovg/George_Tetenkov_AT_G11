3d17c43de8e2ef44f89481133c167a1882f58421
George Tetenkov <george.tetenkov@gmail.com>
2024-12-20 19:43:18 +0100

    Homework 13 part with locators (mailru) Homework 13 part with Classwork 14

---
+++ src/main/java/classwork/day14/SeleniumTestTwo.java
@@ -0,0 +1,32 @@
+package classwork.day14;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.Thread.sleep;
+
+public class SeleniumTestTwo {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver1 = new ChromeDriver();
+        driver1.get("https://google.com/");
+
+        driver1.findElement(By.name("q")).sendKeys("погода минск");
+        sleep(2000);
+        WebElement firstElement = driver1.findElement(By.xpath("//*[@role='listbox']/*[@data-entityname='погода минск']"));
+        firstElement.click();
+        sleep(500);
+
+        WebElement tomorrow = driver1.findElement(By.xpath("//*[@id='wob_gsvg']/text[25]"));
+        sleep(500);
+        tomorrow.click();
+
+
+
+        System.out.println(driver1.getTitle());
+        System.out.println(driver1.getCurrentUrl());
+        driver1.quit();
+    }
+}
