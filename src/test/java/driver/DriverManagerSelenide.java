e3fb55c94fc028702f128c2a421fe025bfac8939
George Tetenkov <george.tetenkov@gmail.com>
2025-01-14 17:53:33 +0100

    classwork_day 20 Selenide driver maximized

--- src/test/java/driver/DriverManagerSelenide.java
+++ src/test/java/driver/DriverManagerSelenide.java
@@ -2,13 +2,13 @@ package driver;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManagerSelenide {
 
     public static void setUp() {
-        Configuration.browser = "chrome";
-        //Configuration.startMaximized = true;
-        Configuration.timeout = 5000;
+        ChromeOptions caps = new ChromeOptions();
+        caps.addArguments("start-maximized");
     }
 
     public static void tearDown() {
